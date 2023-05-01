f943f9bdb9aeefc326e85bbc568583198189db42
wanqian99 <gaowanqian.99@gmail.com>
2023-05-01 18:54:50 +0800

    refactor APOD api call to new file

--- src/components/Apod.tsx
+++ src/components/Apod.tsx
@@ -8,42 +8,14 @@ import {
 	Typography,
 	ImageListItem,
 } from "@mui/material";
-
-interface APOD {
-	copyright: string;
-	date: string;
-	explanation: string;
-	title: string;
-	url: string;
-}
+import useAPOD from "../hooks/useAPOD";
 
 const Apod = () => {
-	const [data, setData] = useState<APOD>();
-	const [error, setError] = useState("");
-	const [isLoading, setLoading] = useState(false);
-
-	useEffect(() => {
-		const controller = new AbortController();
-		axios
-			.get(
-				"https://api.nasa.gov/planetary/apod?api_key=dbOSX0vcEIjTHSeQhgvm3y6WOZDHDHCBQJG2toMo",
-				{ signal: controller.signal }
-			)
-			.then((res) => {
-				setData(res.data);
-				setLoading(false);
-				console.log(res.data);
-			})
-			.catch((err) => {
-				if (err instanceof CanceledError) return;
-				setError(err.message);
-				setLoading(false);
-			});
-		return () => controller.abort();
-	}, []);
+	const { data, error, isLoading } = useAPOD();
 
 	if (error) return <p>{error}</p>;
 	if (isLoading) return <p>Loading Picture of the Day...</p>;
+
 	return (
 		<>
 			<Box
---
+++ src/hooks/useAPOD.ts
@@ -0,0 +1,39 @@
+import axios, { CanceledError } from "axios";
+import { useState, useEffect } from "react";
+
+
+export interface APOD {
+	copyright: string;
+	date: string;
+	explanation: string;
+	title: string;
+	url: string;
+}
+
+const useAPOD = () => {
+    const [data, setData] = useState<APOD>();
+	const [error, setError] = useState("");
+	const [isLoading, setLoading] = useState(false);
+
+	useEffect(() => {
+		const controller = new AbortController();
+		axios
+			.get(
+				"https://api.nasa.gov/planetary/apod?api_key=dbOSX0vcEIjTHSeQhgvm3y6WOZDHDHCBQJG2toMo",
+				{ signal: controller.signal }
+			)
+			.then((res) => {
+				setData(res.data);
+				setLoading(false);
+			})
+			.catch((err) => {
+				if (err instanceof CanceledError) return;
+				setError(err.message);
+				setLoading(false);
+			});
+		return () => controller.abort();
+	}, []);
+
+    return { data, error, isLoading };
+}
+export default useAPOD;
