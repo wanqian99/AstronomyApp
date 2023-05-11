e8cf0e268d0f8c1a361bb0d2a4d78d1d7012086c
wanqian99 <gaowanqian.99@gmail.com>
2023-05-11 22:57:00 +0800

    update client vercel.json

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,14 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     server: {
-        proxy: {
-            '/api': {
-                 target: 'https://astronomy-app-api.vercel.app',
-                 changeOrigin: true,
-                 secure: false,      
-                 ws: true,
-             }
-        }
+        proxy: { "/api": "https://astronomy-app-api.vercel.app"},
     },
   plugins: [react()],
 })
